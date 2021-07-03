defedd2dfa3aa67f3b6ccd8fe7692086ff8ed36c
Arun Jamal <ajamal31@hotmail.com>
2021-07-02 19:35:07 -0600

    Format Files

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require('electron')
 
+// Load index.html into a new BrowserWindows instance
 function createWindow() {
     const window = new BrowserWindow({
         width: 800,
@@ -9,6 +10,7 @@ function createWindow() {
     window.loadFile('index.html')
 }
 
+// Launch Window after the app module's ready event is fired
 app.whenReady().then(() => {
     createWindow()
 })
