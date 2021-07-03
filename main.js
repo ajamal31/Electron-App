bae5ffcc37162aa5a5a059ba875ab92c95774ac3
Arun Jamal <ajamal31@hotmail.com>
2021-07-03 15:41:24 -0600

    Access Node.js from the renderer with a preload script

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 // Load index.html into a new BrowserWindows instance
 function createWindow() {
     const window = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
     })
 
     window.loadFile('index.html')
