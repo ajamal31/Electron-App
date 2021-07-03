c7e5c5451f176b7cc86e2bb1bd7c15ea4c6dd8fb
Arun Jamal <ajamal31@hotmail.com>
2021-07-03 16:29:51 -0600

    Load my Weather App

--- main.js
+++ main.js
@@ -4,14 +4,15 @@ const path = require('path')
 // Load index.html into a new BrowserWindows instance
 function createWindow() {
     const window = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1700,
+        height: 1000,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
     })
 
-    window.loadFile('index.html')
+    // window.loadFile('index.html')
+    window.loadURL('https://ajamal31.github.io/Weather-App-ReactJS/')
 }
 
 // Launch Window after the app module's ready event is fired
